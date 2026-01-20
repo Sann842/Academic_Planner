d140c3c9e376d9b5d7cf8458ce2d87a7ffb4e76a
Sann842 <karkisanchu06@gmail.com>
2026-01-20 16:32:15 +0545

    Revert " adjustements for deployment, mainly URL intergration"
    This reverts commit 1e002c97e8a04a176a333443bd38578d9dd20c73.

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,4 @@
-//const API_BASE_URL = "http://127.0.0.1:8000/api";
-const API_BASE_URL = "https://academic-planner-1rjz.onrender.com/api";
+const API_BASE_URL = "http://127.0.0.1:8000/api";
 
 // -------------------------
 // Token management
